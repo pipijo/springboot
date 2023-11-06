b7622bfd7a6e42c6f925d79a271317a2d29f05ba
pipijo <pipij782@gmail.com>
2023-11-06 20:46:53 +0800

    补交

---
+++ src/main/java/com/yunhe/SpringBootSsmApplication.java
@@ -0,0 +1,17 @@
+package com.yunhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author sinue
+ */
+@SpringBootApplication
+public class SpringBootSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSsmApplication.class, args);
+    }
+
+}
